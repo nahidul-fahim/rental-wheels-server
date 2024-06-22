a26e8d6142d1fa2847459ce4d551d47d366eafea
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-06-22 23:37:11 +0600

    updated model

--- src/app/modules/User/user.model.ts
+++ src/app/modules/User/user.model.ts
@@ -5,12 +5,12 @@ import { TUser } from "./user.interface";
 const userSchema = new Schema<TUser>({
     name: {
         type: String,
-        required: true,
+        required: [true, 'Name is required'],
         trim: true,
     },
     email: {
         type: String,
-        required: true,
+        required: [true, 'Email is required'],
         unique: true,
         trim: true,
     },
@@ -20,16 +20,17 @@ const userSchema = new Schema<TUser>({
     },
     password: {
         type: String,
-        required: true,
+        required: [true, 'Password is required'],
+        minlength: [6, "Password must be at least 6 characters long"]
     },
     phone: {
         type: String,
-        required: true,
+        required: [true, 'Phone is required'],
         trim: true,
     },
     address: {
         type: String,
-        required: true,
+        required: [true, 'Address is required'],
         trim: true,
     }
 }, { timestamps: true });
