ecde3bce83609b0f40d5527ca339ff5047c9d579
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-06-24 23:05:19 +0600

    created pre and post middlewaare in user model to hash password and hide from client

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -7,5 +7,5 @@ export default ({
     NODE_ENV: process.env.NODE_ENV,
     port: process.env.PORT,
     db_url: process.env.DATABASE_URL,
-
+    bcrypt_salt_round: process.env.BCRYPT_SALT,
 })
--- src/app/modules/User/user.model.ts
+++ src/app/modules/User/user.model.ts
@@ -1,5 +1,7 @@
 import { Schema, model } from "mongoose";
 import { TUser } from "./user.interface";
+import bcrypt from "bcrypt";
+import config from "../../config";
 
 
 const userSchema = new Schema<TUser>({
@@ -35,6 +37,24 @@ const userSchema = new Schema<TUser>({
     }
 }, { timestamps: true });
 
+
+// hash password before saving to db
+userSchema.pre('save', async function (next) {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const user = this;
+    user.password = await bcrypt.hash(
+        user.password,
+        Number(config.bcrypt_salt_round)
+    );
+    next();
+})
+
+
+// set "" after saving password
+userSchema.post('save', async function (doc, next) {
+    doc.password = "";
+    next();
+})
 
 
 export const User = model<TUser>('user', userSchema)
