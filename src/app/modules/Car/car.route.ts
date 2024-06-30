ea68d9ff6db805640745c1a819cfa2a15579ce20
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-06-30 21:42:05 +0600

    created transition and rollback for car booking

--- src/app/modules/Car/car.route.ts
+++ src/app/modules/Car/car.route.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import auth from "../../middlewares/auth";
-import { USER_ROLE } from "../User/user.constant";
 import { CarValidation } from "./car.validation";
 import { CarController } from "./car.controller";
+import { USER_ROLE } from "../user/user.constant";
 
 
 const router = Router();
--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -2,6 +2,8 @@ import httpStatus from "http-status";
 import AppError from "../../errors/AppError";
 import { TCarBooking } from "./booking.interface";
 import { Booking } from "./booking.model";
+import { Car } from "../car/car.model";
+import mongoose from "mongoose";
 
 
 // book a car services
@@ -10,17 +12,39 @@ const bookACarIntoDB = async (payload: TCarBooking, user: string) => {
         ...payload,
         user: user,
     };
-    const createdBooking = await Booking.create(bookingData);
-    
-    if (!createdBooking) {
-        throw new AppError(httpStatus.NOT_FOUND, "Failed to create booking")
+
+    const session = await mongoose.startSession();
+
+    try {
+        session.startTransaction();
+
+        const createdBooking = await Booking.create([bookingData], { session });
+        if (!createdBooking) {
+            throw new AppError(httpStatus.NOT_FOUND, "Failed to create booking")
+        }
+        const updateCarStatus = await Car.findByIdAndUpdate(
+            payload.car,
+            { status: 'unavailable' },
+            { new: true, session },
+        )
+        if (!updateCarStatus) {
+            throw new AppError(httpStatus.NOT_FOUND, "Failed to book car!")
+        }
+
+        await session.commitTransaction();
+        await session.endSession();
+
+        const populatedBooking = await Booking.findById(createdBooking[0]._id)
+            .populate('car')
+            .populate({ path: 'user', select: '-createdAt -updatedAt' })
+
+        return populatedBooking;
     }
-
-    const populatedBooking = await Booking.findById(createdBooking._id)
-        .populate('car')
-        .populate({ path: 'user', select: '-createdAt -updatedAt' })
-
-    return populatedBooking;
+    catch (error) {
+        await session.abortTransaction();
+        await session.endSession();
+        throw new AppError(httpStatus.NOT_FOUND, "Failed to book car!")
+    }
 }
 
 
