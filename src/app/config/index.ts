067c295f861146223c81e842a8a4fd705f4b8478
nahidul-fahim <nahidul7562@gmail.com>
2024-09-24 01:18:56 +0600

    added saving image to cloudinary functionality

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -10,4 +10,7 @@ export default ({
     bcrypt_salt_round: process.env.BCRYPT_SALT,
     jwt_access_secret: process.env.JWT_ACCESS_SECRET,
     jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+    cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+    cloudinary_secret_key: process.env.CLOUDINARY_SECRET_KEY,
 })
--- src/app/modules/car/car.route.ts
+++ src/app/modules/car/car.route.ts
@@ -1,9 +1,10 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import auth from "../../middlewares/auth";
 import { CarValidation } from "./car.validation";
 import { CarController } from "./car.controller";
 import { USER_ROLE } from "../user/user.constant";
+import { sendImageToCloudinary, upload } from "../../utils/sendImageToCloudinary";
 
 
 const router = Router();
@@ -12,6 +13,12 @@ const router = Router();
 router.post(
     "/",
     auth(USER_ROLE.admin),
+    upload.single('file'),
+    sendImageToCloudinary,
+    (req: Request, res: Response, next: NextFunction) => {
+        req.body = JSON.parse(req.body.data);
+        next();
+    },
     validateRequest(CarValidation.createCarValidationSchema),
     CarController.createNewCar
 )
